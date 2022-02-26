54c1c986e44d76fb94ad5b2aac172d3664257f95
Luukas <luukas.makila@kolumbus.fi>
2022-02-26 15:05:19 +0200

    added better error handling to helper functions

--- helpers.py
+++ helpers.py
@@ -1,31 +1,40 @@
 import json
 
-from numpy import tri
-
 #contains all the helper functions
 
 #loads data from a JSON file (could be a db in production)
 async def load_data(path: str):
-  data = json.load(open(path))
-  return data
+  try:
+    data = json.load(open(path))
+    return data
+  except:
+    return False
 
 #books a trip
 async def add_trip(booked_trip: dict):
+  try:
+    with open('trips.json', 'r') as file:
+      data = json.load(file)
+    
+    data.append(booked_trip)
 
-  with open('trips.json', 'r') as file:
-    data = json.load(file)
-  
-  data.append(booked_trip)
-
-  with open('trips.json', 'w') as file:
-    json.dump(data, file)
-    file.close()
+    with open('trips.json', 'w') as file:
+      json.dump(data, file)
+      file.close()
+  except:
+    return False
 
 async def get_one_trip(tripId: str):
 
-  trips = await load_data('trips.json')
-  
-  for trip in trips:
-    if trip['id'] == tripId:
-      return trip
-  return False
+  try:
+    #Load all trip info
+    trips = await load_data('trips.json')
+    
+    #Query for uuid
+    for trip in trips:
+      if trip['id'] == tripId:
+        return trip
+
+    return False
+  except:
+    return False
