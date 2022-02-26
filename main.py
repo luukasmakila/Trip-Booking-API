8bdc01236be413fbc51c087626d756d6a1d31b92
Luukas <luukas.makila@kolumbus.fi>
2022-02-26 13:57:13 +0200

    added filtering to destinations with query params

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 import helpers
 
 #initializes the app
@@ -6,6 +7,19 @@ app = FastAPI()
 
 #API endpoints
 @app.get('/api/destinations')
-async def get_destinations():
+async def get_destinations(maxTemp: Optional[int] = None, minTemp: Optional[int] = None, type: Optional[str] = None):
+
+  #get destination data
   destinations = await helpers.load_data('planets.json')
+
+  #query based on if parameters are given
+  if maxTemp:
+    destinations = [destination for destination in destinations if destination['avgTemp'] <= maxTemp]
+  if minTemp:
+    destinations = [destination for destination in destinations if destination['avgTemp'] >= minTemp]
+  if type and type == 'planet':
+    destinations = [destination for destination in destinations if destination['isPlanet'] == True]
+  if type and type == 'moon':
+    destinations = [destination for destination in destinations if destination['isPlanet'] == False]
+
   return {'destinations': destinations}
